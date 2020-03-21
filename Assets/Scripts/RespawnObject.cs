a2e78689525dafbc0f128675f5491434c4d9f928
Guilherme <guilherme_campiotto@hotmail.com>
2020-03-21 17:57:24 -0300

    Indicator when a spike is falling and assets for walls.
    Signed-off-by: Guilherme <guilherme_campiotto@hotmail.com>

--- Assets/Scripts/RespawnObject.cs
+++ Assets/Scripts/RespawnObject.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RespawnObject : MonoBehaviour
 {
     public GameObject prefab;
+    public GameObject indicator1;
+    public GameObject indicator2;
+    public GameObject indicator3;
     public GameController gameController;
     private float inicialPositionX;
     private float inicialPositionY;
@@ -16,6 +19,8 @@ public class RespawnObject : MonoBehaviour
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
         inicialPositionX = transform.position.x;
         inicialPositionY = transform.position.y;
+
+        RespawnIndicators(inicialTime);
         InvokeRepeating("Respawn", inicialTime, respawnTime);
     }
 
@@ -24,6 +29,23 @@ public class RespawnObject : MonoBehaviour
         if(!gameController.levelComplete)
         {
             Instantiate(prefab, new Vector2(inicialPositionX, inicialPositionY), Quaternion.identity);
+            RespawnIndicators(respawnTime);
         }
+    }
+
+    void RespawnIndicators(float time)
+    {
+        if(indicator1 != null && indicator2 != null && indicator3 != null)
+        {
+            StartCoroutine(RespawnIndicator(time * 0.6f, indicator1, 0.1f));
+            StartCoroutine(RespawnIndicator(time * 0.7f, indicator3, - 0.1f));
+            StartCoroutine(RespawnIndicator(time * 0.8f, indicator2, 0.1f));
+        }
+    }
+
+    IEnumerator RespawnIndicator(float time, GameObject indicator, float moveVariation)
+    {
+        yield return new WaitForSeconds(time);
+        Instantiate(indicator, new Vector2(inicialPositionX + moveVariation, inicialPositionY), Quaternion.identity);
     }
 }
